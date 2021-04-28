cf8dd17796732fe787ca554c859c673843d42c3a
Angela Csomo <angela.csomo@gmail.com>
2021-04-28 15:54:11 +0200

    fetchből function

--- src/App.js
+++ src/App.js
@@ -3,27 +3,27 @@ import "./App.css";
 
 const App = () => {
   const [search, setSearch] = useState("");
-  //console.log(search)
-
-  // const [isLoading, setIsLoading] = useState(false);
   const [data, setdata] = useState([]);
 
-  useEffect(() => {
+  function click() {
     //setLoading(true);
 
     fetch(`/api/clients?search=${search}`)
       .then((response) => response.json())
       .then((data) => setdata(data))
       .catch((error) => setdata("hiba"));
-    //  .finally(() => setLoading(false));
-  },[]);
+  }
 
-  console.log(data);
   return (
     <div className="App">
       <h1>Veterinarian admin - clients</h1>
       <input type="text" onChange={(e) => setSearch(e.target.value)} />
-      <button disabled={search.length < 3} ocClick={() => ()}>Search</button>
+      <button
+        disabled={search.length < 3 ? true : false}
+        onClick={() => click()}
+      >
+        Search
+      </button>
     </div>
   );
 };
