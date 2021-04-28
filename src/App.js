718790bd86b5d6fabb7ac244a86b9278d877a8bc
Angela Csomo <angela.csomo@gmail.com>
2021-04-28 15:33:50 +0200

    Client.js setup

--- src/App.js
+++ src/App.js
@@ -1,36 +1,31 @@
-import React, {useState, useEffect} from "react";
-import './App.css'
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 const App = () => {
+  const [search, setSearch] = useState("");
+  //console.log(search)
 
-  const [search, setSearch] = useState('')
-  console.log(search)
-  function App() {
-   // const [isLoading, setIsLoading] = useState(false);
-    const [data, setdata] = useState([]);
-  
-  
-    useEffect(() => {
-      //setLoading(true);
-  
-      fetch(`/api/clients?search=${search}`)
-        .then((response) => response.json())
-        .then((data) => setdata(data))
-        .catch((error) => setdata(null))
-      //  .finally(() => setLoading(false));
-    }, []);
-  }
+  // const [isLoading, setIsLoading] = useState(false);
+  const [data, setdata] = useState([]);
 
+  useEffect(() => {
+    //setLoading(true);
+
+    fetch(`/api/clients?search=${search}`)
+      .then((response) => response.json())
+      .then((data) => setdata(data))
+      .catch((error) => setdata("hiba"));
+    //  .finally(() => setLoading(false));
+  },[]);
+
+  console.log(data);
   return (
     <div className="App">
-    <h1>Veterinarian admin - clients</h1>
-    <input type="text" onChange={(e) => setSearch(e.target.value)} />
-      <button disabled={search.length < 3}>Search</button>
-
-   
-
+      <h1>Veterinarian admin - clients</h1>
+      <input type="text" onChange={(e) => setSearch(e.target.value)} />
+      <button disabled={search.length < 3} ocClick={() => ()}>Search</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
