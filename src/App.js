e9e6fbfe1082ebf9b01c36f930e1be22a3e305dc
Angela Csomo <angela.csomo@gmail.com>
2021-04-28 14:54:08 +0200

    initial steps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React, {useState, useEffect} from "react";
 import './App.css'
 
 const App = () => {
 
+  const [search, setSearch] = useState('')
+
   return (
     <div className="App">
-      Todo...
+    <h1>Veterinarian admin - clients</h1>
+    <input type="text" />
+      <button>Search</button>
+
+   
+
     </div>
   )
 }
